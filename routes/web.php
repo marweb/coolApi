25be26baf2eb423062ab8219e663e4f144837165
Mario Rios <marweb@gmail.com>
2024-07-16 02:01:05 -0500

    modify welcome blade template

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) {
-    return response()->json([
-        "status" => true,
-        "message" => "Cool API v1"
-    ]);
+    return view('welcome');
 });
