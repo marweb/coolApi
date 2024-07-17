d333cc5ad03dbdd72ea4e36f6f20e3789f034692
Mario Rios <marweb@gmail.com>
2024-07-17 01:50:41 -0500

    change css and style soon blade

--- resources/views/soon.blade.php
+++ resources/views/soon.blade.php
@@ -21,43 +21,32 @@
 			integrity="sha256-D9M5yrVDqFlla7nlELDaYZIpXfFWDytQtiV+TaH6F1I="
 			crossorigin="anonymous"
 		/>
-		<link rel="stylesheet" type="text/css" href="css/styles.css" />
 		<link
 			rel="stylesheet"
 			href="https://cdn.jsdelivr.net/npm/bulma-social@1/bin/bulma-social.min.css"
 		/>
 		<script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+            <style>
+            html, body { padding: 0; margin: 0; width: 100%; height: 100%; }
+            * {box-sizing: border-box;}
+            body { text-align: center; padding: 0; background: #207cca; color: #fff; font-family: Open Sans; }
+            body { font-family: Open Sans; font-weight: 100; font-size: 20px; color: #fff; text-align: center; display: -webkit-box; display: -ms-flexbox; display: flex; -webkit-box-pack: center; -ms-flex-pack: center; justify-content: center; -webkit-box-align: center; -ms-flex-align: center; align-items: center;}
+            a { color: #fff; font-weight: bold;}
+            a:hover { text-decoration: none; }
+            svg { width: 75px; margin-top: 1em; }
+            </style>
+
 	</head>
 	<body>
 		<section class="hero is-fullheight">
 			<div class="hero-body">
 				<div class="container has-text-centered">
 					<div class="column is-8 is-offset-2 is-6-widescreen is-offset-3-widescreen">
-						<p class="title is-1">COMING SOON</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 202.24 202.24"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Asset 3</title><g id="Layer_2" data-name="Layer 2"><g id="Capa_1" data-name="Capa 1"><path class="cls-1" d="M101.12,0A101.12,101.12,0,1,0,202.24,101.12,101.12,101.12,0,0,0,101.12,0ZM159,148.76H43.28a11.57,11.57,0,0,1-10-17.34L91.09,31.16a11.57,11.57,0,0,1,20.06,0L169,131.43a11.57,11.57,0,0,1-10,17.34Z"/><path class="cls-1" d="M101.12,36.93h0L43.27,137.21H159L101.13,36.94Zm0,88.7a7.71,7.71,0,1,1,7.71-7.71A7.71,7.71,0,0,1,101.12,125.63Zm7.71-50.13a7.56,7.56,0,0,1-.11,1.3l-3.8,22.49a3.86,3.86,0,0,1-7.61,0l-3.8-22.49a8,8,0,0,1-.11-1.3,7.71,7.71,0,1,1,15.43,0Z"/></g></g></svg>
+						<p class="title is-1 has-text-white">We’ll be back soon!</p>
 						<div class="container area-center">
-							<div class="buttons is-centered">
-								<a class="button is-medium is-facebook" href="#">
-									<span class="icon">
-										<i class="fab fa-facebook-f fa-lg"></i>
-									</span>
-								</a>
-								<a class="button is-medium is-twitter" href="#">
-									<span class="icon">
-										<i class="fab fa-twitter fa-lg"></i>
-									</span>
-								</a>
-								<a class="button is-medium is-youtube" href="#">
-									<span class="icon">
-										<i class="fab fa-youtube fa-lg"></i>
-									</span>
-								</a>
-								<a class="button is-medium is-linkedin" href="#">
-									<span class="icon">
-										<i class="fab fa-linkedin-in fa-lg"></i>
-									</span>
-								</a>
-							</div>
-							<p class="subtitle is-4">
+							<p class="subtitle is-4 has-text-white">
                                 Our site is currently being built, but we’re all set to assist you! Interested in learning more about us, discussing your project, or simply want to say hi? Reach out to us on social media, and we’ll respond as soon as we can.
 							</p>
 							<br />
